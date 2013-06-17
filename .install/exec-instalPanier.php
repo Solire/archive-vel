c35cb9a6bf7b535a999ad061135828acf7cfe8e6
Adrien <aimbert@solire.fr>
2013-06-17 12:12:28 +0200

    Ajout du script de création de l'architecture du panier

---
+++ .install/exec-instalPanier.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template simple de script d'installation
+ *
+ * @package    Slrfw
+ * @subpackage Install
+ * @author     Adrien <aimbert@solire.fr>
+ * @license    Solire http://www.solire.fr/
+ */
+
+set_include_path(
+    get_include_path()
+    . PATH_SEPARATOR . realpath(pathinfo(__FILE__, PATHINFO_DIRNAME) . '/../../')
+);
+
+require 'slrfw/init.php';
+
+\Slrfw\FrontController::init();
+
+$db = \Slrfw\Registry::get('db');
+
+/** Mettre script d'installation ici  **/
+
+$query = 'CREATE TABLE IF NOT EXISTS `boutique_panier` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cle` char(32) NOT NULL,
+  `hit` timestamp NOT NULL DEFAULT "0000-00-00 00:00:00" ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;';
+
+$db->exec($query);
+
+$query = 'CREATE TABLE IF NOT EXISTS `boutique_panier_ligne` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_panier` int(11) unsigned NOT NULL,
+  `id_reference` int(11) unsigned NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_commande` (`id_panier`),
+  KEY `id_reference` (`id_reference`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+
+$db->exec($query);
