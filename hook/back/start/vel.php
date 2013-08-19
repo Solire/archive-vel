af52e41b45453594c467e6f781e4310347873a67
Adrien <aimbert@solire.fr>
2013-08-19 14:23:06 +0200

    Chargement du nombre de commandes dans le main du back

---
+++ hook/back/start/vel.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Chargement des informations sur les commandes
+ *
+ * @package    Vel
+ * @subpackage Hook
+ * @author     Adrien <aimbert@solire.fr>
+ * @license    CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+
+namespace Vel\Hook\Back\Start;
+
+/**
+ * Chargement des informations sur les commandes
+ *
+ * @package    Vel
+ * @subpackage Hook
+ * @author     Adrien <aimbert@solire.fr>
+ * @license    CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+class Vel implements \Slrfw\HookInterface
+{
+    /**
+    * Chargement des informations sur les commandes
+    *
+    * @param \Slrfw\Hook $env
+    *
+    * @return void
+    */
+    public function run($env)
+    {
+        $commandeClass = \Slrfw\FrontController::searchClass('Lib\Commande');
+        $commande = new $commandeClass;
+        unset($commandeClass);
+
+
+        $env->ctrl->_view->countCmd = count($commande->listeEnCours());
+    }
+}
+
