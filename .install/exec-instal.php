5fef2a0533c081772963e3d61cd2969e40b2a17f
Adrien <aimbert@solire.fr>
2013-08-28 12:50:01 +0200

    correction chargement des chemins vers les scripts d'installation

--- .install/exec-instal.php
+++ .install/exec-instal.php
@@ -23,13 +23,14 @@ $db = \Slrfw\Registry::get('db');
 
 /** Mettre script d'installation ici  **/
 
-$dir = opendir(pathinfo(__FILE__, PATHINFO_DIRNAME));
+$dirPath = pathinfo(__FILE__, PATHINFO_DIRNAME);
+$dir = opendir($dirPath);
 $me = pathinfo(__FILE__, PATHINFO_BASENAME);
 while ($file = @readdir($dir)) {
     if ($file == '.' || $file == '..' || $file == $me) {
         continue;
     }
-    if (is_dir($dir . DS . $file)) {
+    if (is_dir($dirPath . DS . $file)) {
         continue;
     }
 
@@ -38,7 +39,7 @@ while ($file = @readdir($dir)) {
     }
 
     try {
-        include $dir . DS . $file;
+        include $dirPath . DS . $file;
         echo str_replace('exec-install', '', $file) . "\t\t\033[32m" . '[OK]' . "\033[00m\r\n";
     } catch (\Exception $exc) {
         echo str_replace('exec-install', '', $file) . "\t\t\033[31m" . '[KO]'
